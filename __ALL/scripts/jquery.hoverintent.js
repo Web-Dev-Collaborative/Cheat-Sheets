fe3eb581d2251479c08ed3219a696374c618a4c9
Bryan C Guner <66654881+bgoonz@users.noreply.github.com>
2021-09-03 21:52:44 +0000

    hi

--- __ALL/scripts/jquery.hoverintent.js
+++ __ALL/scripts/jquery.hoverintent.js
@@ -6,81 +6,87 @@
  * means you are free to use hoverIntent as long as this header is left intact.
  * Copyright 2007, 2014 Brian Cherne
  */
-( function ( $ ) {
-  $.fn.hoverIntent = function ( handlerIn, handlerOut, selector ) {
+(function ($) {
+  $.fn.hoverIntent = function (handlerIn, handlerOut, selector) {
     var cfg = {
       interval: 100,
       sensitivity: 6,
-      timeout: 0
+      timeout: 0,
     };
-    if ( typeof handlerIn === "object" ) {
-      cfg = $.extend( cfg, handlerIn )
+    if (typeof handlerIn === "object") {
+      cfg = $.extend(cfg, handlerIn);
     } else {
-      if ( $.isFunction( handlerOut ) ) {
-        cfg = $.extend( cfg, {
+      if ($.isFunction(handlerOut)) {
+        cfg = $.extend(cfg, {
           over: handlerIn,
           out: handlerOut,
-          selector: selector
-        } )
+          selector: selector,
+        });
       } else {
-        cfg = $.extend( cfg, {
+        cfg = $.extend(cfg, {
           over: handlerIn,
           out: handlerIn,
-          selector: handlerOut
-        } )
+          selector: handlerOut,
+        });
       }
     }
     var cX, cY, pX, pY;
-    var track = function ( ev ) {
+    var track = function (ev) {
       cX = ev.pageX;
-      cY = ev.pageY
+      cY = ev.pageY;
     };
-    var compare = function ( ev, ob ) {
-      ob.hoverIntent_t = clearTimeout( ob.hoverIntent_t );
-      if ( Math.sqrt( ( pX - cX ) * ( pX - cX ) + ( pY - cY ) * ( pY - cY ) ) < cfg.sensitivity ) {
-        $( ob ).off( "mousemove.hoverIntent", track );
+    var compare = function (ev, ob) {
+      ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
+      if (
+        Math.sqrt((pX - cX) * (pX - cX) + (pY - cY) * (pY - cY)) <
+        cfg.sensitivity
+      ) {
+        $(ob).off("mousemove.hoverIntent", track);
         ob.hoverIntent_s = true;
-        return cfg.over.apply( ob, [ ev ] )
+        return cfg.over.apply(ob, [ev]);
       } else {
         pX = cX;
         pY = cY;
-        ob.hoverIntent_t = setTimeout( function () {
-          compare( ev, ob )
-        }, cfg.interval )
+        ob.hoverIntent_t = setTimeout(function () {
+          compare(ev, ob);
+        }, cfg.interval);
       }
     };
-    var delay = function ( ev, ob ) {
-      ob.hoverIntent_t = clearTimeout( ob.hoverIntent_t );
+    var delay = function (ev, ob) {
+      ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
       ob.hoverIntent_s = false;
-      return cfg.out.apply( ob, [ ev ] )
+      return cfg.out.apply(ob, [ev]);
     };
-    var handleHover = function ( e ) {
-      var ev = $.extend( {}, e );
+    var handleHover = function (e) {
+      var ev = $.extend({}, e);
       var ob = this;
-      if ( ob.hoverIntent_t ) {
-        ob.hoverIntent_t = clearTimeout( ob.hoverIntent_t )
+      if (ob.hoverIntent_t) {
+        ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
       }
-      if ( e.type === "mouseenter" ) {
+      if (e.type === "mouseenter") {
         pX = ev.pageX;
         pY = ev.pageY;
-        $( ob ).on( "mousemove.hoverIntent", track );
-        if ( !ob.hoverIntent_s ) {
-          ob.hoverIntent_t = setTimeout( function () {
-            compare( ev, ob )
-          }, cfg.interval )
+        $(ob).on("mousemove.hoverIntent", track);
+        if (!ob.hoverIntent_s) {
+          ob.hoverIntent_t = setTimeout(function () {
+            compare(ev, ob);
+          }, cfg.interval);
         }
       } else {
-        $( ob ).off( "mousemove.hoverIntent", track );
-        if ( ob.hoverIntent_s ) {
-          ob.hoverIntent_t = setTimeout( function () {
-            delay( ev, ob )
-          }, cfg.timeout )
+        $(ob).off("mousemove.hoverIntent", track);
+        if (ob.hoverIntent_s) {
+          ob.hoverIntent_t = setTimeout(function () {
+            delay(ev, ob);
+          }, cfg.timeout);
         }
       }
     };
-    return this.on( {
-      "mouseenter.hoverIntent": handleHover,
-      "mouseleave.hoverIntent": handleHover
-    }, cfg.selector )
-  }
-} )( jQuery );
+    return this.on(
+      {
+        "mouseenter.hoverIntent": handleHover,
+        "mouseleave.hoverIntent": handleHover,
+      },
+      cfg.selector
+    );
+  };
+})(jQuery);
